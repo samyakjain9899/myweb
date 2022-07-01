bc7cb938ac5721f45c6d62a6dc7bf3cf66125267
samyakjain9899 <64709124+samyakjain9899@users.noreply.github.com>
2022-07-01 18:36:25 +0530

    Delete silportal directory

--- silportal/src/main/java/com/simactivation/Enum/IdType.java
+++
@@ -1,6 +0,0 @@
-package com.simactivation.Enum;
-
-public class IdType {
-
-
-}
--- silportal/src/main/java/com/simactivation/Enum/Id_Type.java
+++
@@ -1,7 +0,0 @@
-package com.simactivation.Enum;
-
-public enum Id_Type {
-	
-   AADHAR,PANCARD,PASSPORT,VOTERID,DRIVINGLICENCE
-   
-}
