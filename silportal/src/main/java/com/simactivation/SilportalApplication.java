fd2253c7aab24d069725410c25acf4da7a684fb3
samyakjain9899 <64709124+samyakjain9899@users.noreply.github.com>
2022-07-01 18:35:40 +0530

    Add files via upload

---
+++ silportal/src/main/java/com/simactivation/SilportalApplication.java
@@ -0,0 +1,13 @@
+package com.simactivation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilportalApplication.class, args);
+	}
+
+}
