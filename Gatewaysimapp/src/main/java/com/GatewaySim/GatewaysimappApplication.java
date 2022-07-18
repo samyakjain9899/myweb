242a44192770e1c7e4f7c965670324df8d1615f4
samyakjain9899 <64709124+samyakjain9899@users.noreply.github.com>
2022-07-18 20:57:06 +0530

    Add files via upload

---
+++ Gatewaysimapp/src/main/java/com/GatewaySim/GatewaysimappApplication.java
@@ -0,0 +1,13 @@
+package com.GatewaySim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewaysimappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewaysimappApplication.class, args);
+	}
+
+}
---
+++ Gatewaysimapp/src/test/java/com/GatewaySim/GatewaysimappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GatewaySim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaysimappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
