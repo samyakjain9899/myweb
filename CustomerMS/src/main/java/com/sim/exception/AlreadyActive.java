92f0c2d84d656f56ea613e9dc90d1c0756ea36c3
samyakjain9899 <64709124+samyakjain9899@users.noreply.github.com>
2022-07-22 22:52:32 +0530

    Delete CustomerMS directory

--- CustomerMS/src/main/java/com/sim/exception/AlreadyActive.java
+++
@@ -1,9 +0,0 @@
-package com.sim.exception;
-
-public class AlreadyActive extends Exception {
-	 
-		private static final long serialVersionUID = 1L;
-		public AlreadyActive(String errors) {
-			super(errors);
-		}
-}
--- CustomerMS/src/main/java/com/sim/exception/InvalidValuesException.java
+++
@@ -1,9 +0,0 @@
-package com.sim.exception;
-
-public class InvalidValuesException extends Exception {
-	 
-		private static final long serialVersionUID = 1L;
-		public InvalidValuesException(String errors) {
-			super(errors);
-		}
-}
